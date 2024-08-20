4e15465f995ddca28ee9d624c463837171ef2119
unknown <bror.beppe.persson@gmail.com>
2024-08-20 15:27:40 +0200

    add states

--- oneButtonFighter.py
+++ oneButtonFighter.py
@@ -26,10 +26,14 @@ def loadImage(path):
     
     return image
 
-def load_animation(animation_name, nr):
+def load_animation(animation_name, nr = None):
+    da_path = os.path.join(ASSET_FOLDER_NAME, "textures","player", animation_name)
+    if nr==None:
+        lst = os.listdir(da_path)
+        nr = len(lst)
     anim = []
     for i in range(nr):
-        image = pygame.image.load(os.path.join(ASSET_FOLDER_NAME, "textures","player", animation_name, str(i)+".png"))
+        image = pygame.image.load(os.path.join(da_path, str(i)+".png"))
         anim.append(image)
     #cls.animations[animation_name] = anim
     return anim
@@ -70,6 +74,7 @@ class Game():
         self.currentScreenShake = (0,0)
         self.screenshakeDir = (0,0)
         self.freeze_frames = 0
+        self.projectiles = []
 
     def update(self):
 
@@ -79,11 +84,14 @@ class Game():
         else:
             pressed = pygame.key.get_pressed()
             for i in range(2):
-                AI = 0
+                AI = 1
                 if AI and i==1: 
                      self.players[i].update(random.random() < 0.5)
                 else:
                     self.players[i].update(pressed[self.player_controls[i][0]]) # random order?
+
+            for i in self.projectiles:
+                i.update()
 
         if self.screenshake>0:
             ampl = 2 
@@ -94,9 +102,16 @@ class Game():
             self.screenshake = 0
 
     def draw(self):
-        self.canvas.fill((110,110,110))
+        if self.freeze_frames:
+            color = (150,110,110)
+        else:
+            color = (110,110,110)
+        self.canvas.fill(color)
         for i in range(2):
             self.players[i].draw()
+
+        for i in self.projectiles:
+            i.draw()
 
     def shakeScreen(self, amount, side):
         self.screenshake = max(self.screenshake, amount)
@@ -114,8 +129,10 @@ class Game():
 class Player():
 
     animations = {}
-    for i in [("idle",1), ("windup",3), ("charging",4), ("shield",1), ("punch",5), ("hurt",3)]:
-        animations[i[0]] = load_animation(*i)
+    da_path = os.path.join(ASSET_FOLDER_NAME, "textures","player")
+    all_animation_names = os.listdir(da_path)
+    for i in all_animation_names:
+        animations[i] = load_animation(i)
 
     def __init__(self, side):
         self.side = side
@@ -125,6 +142,9 @@ class Player():
 
         self.state = "idle"
         self.stateTimer = 0
+
+        self.state_after_stun = "idle"
+        self.gun_ammo = 1
 
     def update_state(self, nr):
         self.state = nr
@@ -141,47 +161,122 @@ class Player():
         match self.state:
             case "idle":
                 self.update_animation(0)
+                self.state_after_stun = "idle"
                 if pressed:
                     self.update_state("windup")
             case "windup":
-                self.update_animation(self.stateTimer*3//12)
-                if self.stateTimer == 11:
+                self.update_animation(self.stateTimer*3//10)
+                if self.stateTimer == 9:
                     if pressed:
                         self.update_state("charging")
                     else:
                         self.update_state("shield")
             case "charging":
-                self.update_animation(self.stateTimer*4//140)
-                if self.stateTimer == 139 or not pressed:
-                    self.update_state("punch")
+                self.update_animation(self.stateTimer*4//160)
+                if not pressed:
+                    if self.stateTimer < 120:
+                        self.update_state("punch")
+                    else:
+                        self.update_state("firepunch")
             case "shield":
                 self.update_animation(0)
                 if self.stateTimer > 10 and pressed:
                     self.update_state("idle") #change this
             case "punch":
-                self.update_animation(self.stateTimer*5//21)
-                if self.stateTimer == 16:
+                self.update_animation(self.stateTimer*5//20)
+                if self.stateTimer == 13:
                     game.players[not self.side].hurt(0.1, 14)
+                    if pressed:
+                        self.update_state("handspike")
                 if self.stateTimer == 40:
+                    self.update_state("idle")
+            case "firepunch":
+                self.update_animation(self.stateTimer*5//25)
+                if self.stateTimer == 16:
+                    game.players[not self.side].hurt(0.15, 24)
+                if self.stateTimer == 45:
                     self.update_state("idle")
             case "hurt":
                 self.update_animation(self.stateTimer*3//(self.stun+1))
                 if self.stateTimer >= self.stun:
+                    self.update_state(self.state_after_stun)
+                if self.stateTimer == int(self.stun*0.66) and pressed:
+                    self.update_state("skullbash")
+            case "skullbash":
+                self.update_animation(self.stateTimer*4//32)
+                if self.stateTimer == 18:
+                    dmg = self.stun * 0.005 + 0.05
+                    game.players[not self.side].hurt(dmg, 10 + self.stun)
+                    #game.players[not self.side].hurt(0.1, 14)
+                if self.stateTimer == 40:
+                    self.update_state(self.state_after_stun)
+            case "handspike":
+                self.update_animation(self.stateTimer*4//30)
+                if self.stateTimer == 8:
+                    game.players[not self.side].hurt(0.05, 4)
+                    self.state_after_stun = "spikeidle"
+                    self.hurt(0.05, 0)
+                if self.stateTimer >= 30:
+                    self.update_state("spikeidle")
+            case "spikeidle":
+                self.update_animation((self.stateTimer//6) %2)
+                self.state_after_stun = "spikeidle"
+                if pressed:
+                    self.update_state("drill")
+            case "drill":
+                self.update_animation(self.stateTimer*4//40)
+                if self.stateTimer == 10 and not pressed:
+                    self.update_state("spiketoss")
+                if self.stateTimer in [23,30,37]:
+                    game.players[not self.side].hurt(0.02, 0)
+                if self.stateTimer >= 40:
+                    if pressed:
+                        self.stateTimer = 20
+                    else:
+                        self.update_state("spikeidle")
+            case "spiketoss":
+                self.update_animation(self.stateTimer*3//20)
+                if self.stateTimer == 7:
+                    game.projectiles.append(SpikeProjectile(self.side))
+                    self.state_after_stun = "idle"
+                if self.stateTimer == 20:
                     self.update_state("idle")
+            case "gunidle":
+                self.update_animation(0)
+                self.state_after_stun = "gunidle"
+                if pressed:
+                    if self.gun_ammo > 0:
+                        self.update_state("gunshot")
+                    else:
+                        self.update_state("reload")
+            case "gunshot":
+                self.update_animation(self.stateTimer*2//10)
+                if self.stateTimer == 2:
+                    self.gun_ammo -= 1
+                    game.players[not self.side].hurt(0.2, 20)
+                if self.stateTimer == 25:
+                    self.update_state("gunidle")
+            case "reload":
+                self.update_animation(self.stateTimer*5//50)
+                if self.stateTimer == 30:
+                    self.gun_ammo = 1
+                if self.stateTimer == 50:
+                    self.update_state("gunidle")
 
     def hurt(self, dmg, stun):
         if self.state in ["shield","charging"]:
             self.hp -= dmg/10
             playWallSound()
-            game.add_lag(10)
-            game.shakeScreen(10, self.side - 0.5)
+            game.add_lag(stun//2+1)
+            game.shakeScreen(stun//2+1, self.side - 0.5)
         else:
             self.hp = self.hp - dmg
-            self.update_state("hurt")
-            self.stun = stun
+            if stun > 0:
+                self.update_state("hurt")
+                self.stun = stun
             playCrushSound()
-            game.add_lag(10)
-            game.shakeScreen(10, self.side - 0.5)
+            game.add_lag(stun//2+1)
+            game.shakeScreen(stun//2+1, self.side - 0.5)
 
     def draw(self):
         blitting_pos = (self.side*64, 0)
@@ -189,8 +284,42 @@ class Player():
             img = pygame.transform.flip(self.image, True, False)
         else:
             img = self.image
+        pygame.draw.rect(game.canvas, (40,20,20), (blitting_pos[0], blitting_pos[1], 64, 2))
+        pygame.draw.rect(game.canvas, (20,180,20), (blitting_pos[0], blitting_pos[1], 64*self.hp, 2))
         game.canvas.blit(img, blitting_pos)
 
+class Projectile():
+
+    def __init__(self, side):
+        self.age = 0
+        self.side = side
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+class SpikeProjectile(Projectile):
+
+    def __init__(self, side):
+        super().__init__(side)
+        self.image = loadImage("spike.png")
+        if self.side == 1:
+            self.image = pygame.transform.flip(self.image, True, False)
+
+    def update(self):
+        self.age += 1
+
+        if self.age >= 20:
+            if self in game.projectiles:
+                game.projectiles.remove(self)
+            game.players[not self.side].hurt(0.05, 4)
+
+    def draw(self):
+        x = 64 + (self.age - 5)*2 * (self.side-0.5)*-2
+        y = 5 + (self.age-12)**2 * 0.2
+        game.canvas.blit(self.image, (x-16, y-16))
 
 
 game = Game()
